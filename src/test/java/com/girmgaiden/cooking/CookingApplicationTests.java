20657072aa36d0f167a857ab7bd6801b09b59f57
Enol <enol.garcia.zapico@gmail.com>
2024-04-21 17:34:05 +0200

    Added PostgreSQL TestContainer to pass default test

--- src/test/java/com/girmgaiden/cooking/CookingApplicationTests.java
+++ src/test/java/com/girmgaiden/cooking/CookingApplicationTests.java
@@ -2,9 +2,19 @@ package com.girmgaiden.cooking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class CookingApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
 
 	@Test
 	void contextLoads() {
